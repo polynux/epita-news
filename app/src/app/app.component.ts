6a03b10c091a22fc6e1176ffbe8aa19c3583ac10
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-05-18 21:18:41 +0200

    use newsgroup service

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NewsgroupService } from './services/newsgroup.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'epita-news';
+  groups = [];
+
+  constructor(private newsgroupService: NewsgroupService) {
+    newsgroupService.getGroups().subscribe((res) => {
+      this.groups.push(res);
+    });
+  }
 }
