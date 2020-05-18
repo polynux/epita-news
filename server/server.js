a6e458bb88c351980f581e944f6e7eb32f0b8eb6
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-05-18 18:34:57 +0200

    First commit

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const Client = require("newsie").default;
+const client = new Client({
+  host: "news.epita.fr"
+});
+
+const app = require("express")();
+
+app.get("/groups/:group", (req, res) => {
+  listMessagesInGroup(req.params.group)
+    .then(snap => res.send(snap))
+    .catch(err => console.error(err));
+});
+
+app.get("/groups", (req, res) => {
+  listAllGroups()
+    .then(snap => res.send(snap))
+    .catch(err => console.error(err));
+});
+
+app.get("/", (req, res) => {
+  res.send("Rien");
+});
+
+app.listen(8000);
+
+function listAllGroups() {
+  return client.connect().then(res => {
+    return client.list().then(res => {
+      let group = [];
+      for (let i = 0; i < Object.keys(res.newsgroups).length; i++) {
+        group.push(res.newsgroups[i].name);
+      }
+      return group;
+    });
+  });
+}
+
+function listMessagesInGroup(group) {
+  return client
+    .connect()
+    .then(() => {
+      return client.group(group).then(res => {
+        let messages = [];
+        for (let i = res.group.low; i < res.group.high; i++) {
+          messages.push(
+            client
+              .body(i)
+              .then(res => res.article.body.join())
+              .catch(() => messages.slice(i, 1))
+          );
+        }
+        return Promise.all(messages);
+      });
+    })
+    .catch(err => console.error(err));
+}
