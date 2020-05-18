0003369f4b303722642f53e30706fd5d0459b573
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-05-18 21:17:00 +0200

    implements newsgroup service

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { NewsgroupService } from './services/newsgroup.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [NewsgroupService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ app/src/app/services/newsgroup.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsgroupService {
+  constructor(private httpClient: HttpClient) {}
+
+  getGroups() {
+    return this.httpClient.get('http://localhost:8000/groups');
+  }
+}
